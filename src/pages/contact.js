b0573939f242ee34ea9384b7d622cd59cd7807fc
Himali Patel <himali.patel@multidots.com>
2019-03-26 13:19:52 +0530

    added files

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,51 +2,61 @@ import React, { Component } from "react"
 import Helmet from "react-helmet"
 import Layout from "../components/layout"
 import {graphql } from "gatsby"
+import ContactImg from "../images/img-01.png"
 
 
 export class Contact extends Component {
   render() {
     const siteTitle = this.props.data.site.siteMetadata.title
+   
     return (
-      <div>
-      <Helmet title={siteTitle} />
-      <Layout>
-        <section className="about-us pad-70">
-          <div className="container">
-            
-                <div className="row">
-                    <div className="col-lg-4">
-                        <div className="image-container">
-                              
+      <div className="contactImage">
+        <div className="container-contact100">
+        <Helmet title={siteTitle} />
+        <Layout>
+          <section className="contact-us pad-70">
+            <div className="container">
+              
+                  <div className="col-lg-12">     
+                    <div className="wrap-contact100">           
+                        <div className="col-lg-12">                        
+                        <div class="contact100-pic">
+				                          	<img src={ContactImg} alt="IMG" />
+				                 </div>
+                            <form name="contact" method="post" data-netlify="true" className="contact-form" data-netlify-honeypot="bot-field" action="/success">
+                                    <input type="hidden" name="form-name" value="contact" />
+                                    <input type="hidden" name="bot-field" />
+                                    <div>
+                                      <h5>Get in Touch</h5>
+                                    </div>
+                                      <div className="field half first">
+                                          <label htmlFor="name">Name</label>
+                                          <input type="text" name="name" id="name" className="input100"  required/>
+                                          <span class="focus-input100"></span>
+                                      </div>
+                                      <div className="field half">
+                                          <label htmlFor="email">Email</label>
+                                          <input type="text" name="email" id="email" className="input100"  required/>
+                                          <span class="focus-input100"></span>
+                                      </div>
+                                      <div className="field">
+                                          <label htmlFor="message">Message</label>
+                                          <textarea name="message" id="message"   required></textarea>
+                                       
+                                      </div>
+                                      <ul className="actions">
+                                          <li><input type="submit" value="Send Message" className="contact100-form-btn special" /></li>
+                                          <li><input type="reset" value="Clear" className="contact100-clear-btn" /></li>
+                                      </ul>
+                            </form>
                         </div>
-                    </div>
-                    <div className="col-lg-8">
-                    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <input type="hidden" name="bot-field" />
-    <div className="field half first">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-    </div>
-    <div className="field half">
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
-    </div>
-    <div className="field">
-        <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows="6"></textarea>
-    </div>
-    <ul className="actions">
-        <li><input type="submit" value="Send Message" className="special" /></li>
-        <li><input type="reset" value="Clear" /></li>
-    </ul>
-</form>
-                    </div>
-             
+                     </div>   
+              
+              </div>
             </div>
-          </div>
-        </section>
-      </Layout>
+          </section>
+        </Layout>
+        </div>
       </div>
     )
   }
--- src/pages/success.js
+++ src/pages/success.js
@@ -1,24 +1,28 @@
 import React, { Component } from "react"
-
 import Layout from "../components/layout"
+import Link from "gatsby-link"
 
 
 export class Sucess extends Component {
   render() {
 
     return (
-      <div>
-   
-      <Layout>
-        <section className="about-us pad-70">
-          <div className="container">
-            
-                <div className="row">
-                   <h2>Sucess</h2> 
-                 </div>
-          </div>
-        </section>
-      </Layout>
+      <div className="contactImage">
+         <div className="container-contact100">
+          <Layout>
+            <section className="contact-us pad-70">
+              <div className="container text-center">                
+                  <div className="wrap-contact100 ">  
+                       <h4 className="success-msg ">Your information has been submitted Successfully.</h4>
+                       <div className="col-lg-12 text-center" style={{marginTop:'20px'}}>
+                           <Link to="/" className="backtohome contact100-form-btn"> Back To Home </Link>
+                       </div>
+                      
+                  </div>
+              </div>
+            </section>
+          </Layout>
+         </div>
       </div>
     )
   }
