908c8bae2385a319c81330ffaa793bdaf07ba4de
Himali Patel <himali.patel@multidots.com>
2019-03-26 11:09:52 +0530

    added files

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -21,21 +21,25 @@ export class Contact extends Component {
                         </div>
                     </div>
                     <div className="col-lg-8">
-                    <form name="contact" method="POST" data-netlify="true">
-                            <p>
-                                <label>Your Name: <input type="text" name="name" /></label>   
-                            </p>
-                            <p>
-                                <label>Your Email: <input type="email" name="email" /></label>
-                            </p>
-                      
-                            <p>
-                                <label>Message: <textarea name="message"></textarea></label>
-                            </p>
-                            <p>
-                                <button type="submit">Send</button>
-                            </p>
-                        </form>
+                    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                    <input type="hidden" name="bot-field" />
+    <div className="field half first">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+    </div>
+    <div className="field half">
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+    </div>
+    <div className="field">
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message" rows="6"></textarea>
+    </div>
+    <ul className="actions">
+        <li><input type="submit" value="Send Message" className="special" /></li>
+        <li><input type="reset" value="Clear" /></li>
+    </ul>
+</form>
                     </div>
              
             </div>
