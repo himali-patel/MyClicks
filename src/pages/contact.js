d6d744e4f8a70261bc9e662a6839dc300d4bc09e
Himali Patel <himali.patel@multidots.com>
2019-03-26 11:21:19 +0530

    added files

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -22,6 +22,7 @@ export class Contact extends Component {
                     </div>
                     <div className="col-lg-8">
                     <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                    <input type="hidden" name="form-name" value="contact" />
                     <input type="hidden" name="bot-field" />
     <div className="field half first">
         <label htmlFor="name">Name</label>
